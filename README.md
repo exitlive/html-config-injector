e6a9026de3ca741595cad00e752d2f5b32eb4ae0
Erik <e.enomaa@umiami.edu>
2015-04-22 18:15:01 +0200

    FEAT: Make transformer retrieve config key/val.

--- README.md
+++ README.md
@@ -13,23 +13,32 @@ dependency_overrides:
 transformers:
 - exitlive_transformers:
     entry_points: web/index.html
-    config_path: path/to/config
+    config_path: config/default.config.yaml
     config_key: browser_configuration_settings
 ```
 Then when you do `pub build`, you get output like this:
 ```
 entry_points: web/index.html
-config_path: path/to/config
+config_path: config/default.config.yaml
 config_key: browser_configuration_settings
 ```
 from code like this:
 ```
-      print('entry_points: ${config['entry_points']}');
-      print('config_path: ${config['config_path']}');
-      print('config_key: ${config['config_key']}');
+print('entry_points: ${config['entry_points']}');
+print('config_path: ${config['config_path']}');
+print('config_key: ${config['config_key']}');
 ```
 Then if you have something like `BrowserConfig` inside your `web/index.html`,
 say, it will be replaced in the built version (`build/web/index.html`). In this
 proof-of-concept version, it will be replaced by the value of `entry_points`.
+
+Additionally, keys and values under the `browser_configuration_settings` in the
+file specified in `config_path` can be retreived and are currently printed
+(for proof-of-concept). To achieve this, put something like:
+```
+browser_configuration_settings:
+    hello: world
+```
+in `config/default.config.yaml` of the lib that imports the transformer.
 
 This is as far as I've come so far. :)
--- lib/exitlive_transformers.dart
+++ lib/exitlive_transformers.dart
@@ -1,9 +1,12 @@
 library dashboard.transformer;
 
 import 'dart:async';
-import 'package:exitlive_utils/config.dart';
+// import 'package:exitlive_utils/config.dart';
+// The barback package provides tranforming functionality.
 import 'package:barback/barback.dart';
 import 'package:logging/logging.dart';
+// The dart_config package provides config parsing functionality.
+import 'package:dart_config/default_server.dart';
 
 
 var log = new Logger('BrowserConfigTransformer');
@@ -11,8 +14,16 @@ var log = new Logger('BrowserConfigTransformer');
 // Config config2 = new Config.fromFiles('../config/default.config.yaml', '../config/default.config.yaml');
 // bool configValue2 = config2.forwardBuildToVagrant;
 
-
+/// Adds config information to an html page using hidden input fields.
+/// Warning: Use html-safe keys and values to not trash your page.
 class BrowserConfigTransformer extends Transformer {
+  // These three strings make up the hidden input field, in that it is formed as follows:
+  // prefix + key + separator + value + postfix,
+  // ie. <input type="hidden" name="<key>" value="<value>">
+  // There is no input sanitation of any kind.
+  final String prefix = '<input type="hidden" name="';
+  final String separator = '" value="';
+  final String postfix = '">';
   // // List entryPoints;
   // String configPath;
   // String configHeading;
@@ -29,7 +40,9 @@ class BrowserConfigTransformer extends Transformer {
   Future<bool> isPrimary(AssetId id) {
     // print('configValue: ${configValue}');
     // print('configValue2: ${configValue2}');
+    // TODO: Enable logging.
     log.finest('hit0');
+    // TODO: Remove hardcoding (probably by removing endsWith and using entry points).
     return new Future.value(id.path.endsWith('/index.html'));
   }
 
@@ -37,6 +50,13 @@ class BrowserConfigTransformer extends Transformer {
     log.finest('hit');
     // print('HELLO');
     return transform.primaryInput.readAsString().then((content) {
+      // Get config variables to transform into page
+      loadConfig(config['config_path']).then((Map config2) {
+        // TODO: Remove hardcoding.
+        print('here! configs: ${config2['browser_configuration_settings']}');
+      // TODO: Remove print.
+      }, onError: (error) => print(error));
+      // ...
       var id = transform.primaryInput.id;
 
       RegExp configTag = new RegExp(r"BrowserConfig");
@@ -44,6 +64,7 @@ class BrowserConfigTransformer extends Transformer {
       // String contentTransformed = content.replaceAll(configTag, entryPoints[0]);
       String contentTransformed = content.replaceAll(configTag, config['entry_points']);
       // print(contentTransformed);
+      // TODO: Remove prints.
       // Here, a proof of concept:
       print('entry_points: ${config['entry_points']}');
       print('config_path: ${config['config_path']}');
